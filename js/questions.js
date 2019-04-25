b547b9a2dbdde0d9cc06bd2a7a8cf651f9f5ca57
Vasyl <admin@gmail.com>
2019-04-25 20:55:23 +0300

    show in table questions

--- js/questions.js
+++ js/questions.js
@@ -16,7 +16,7 @@ document.getElementById('courses').onclick = function () {
     xhr.send();
 };
 
-function getQuestions() {
+document.getElementById('questions-btn').onclick = function () {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', '/api/questions');
     xhr.onreadystatechange = function () {
@@ -24,16 +24,21 @@ function getQuestions() {
 	    if (xhr.status === 200) {
 		var json_text = xhr.responseText;
 		var questions = JSON.parse(json_text);
-		return questions;
-	    }else{
+		var tbody = document.querySelector('#questions tbody');
+		tbody.innerHTML = '';
+		for (var i = 0; i < questions.length; i++) {
+		    var tr = '<tr>\n\
+<td>' + (i + 1) + '</td>\n\
+<td>' + questions[i].author + '</td>\n\
+<td>' + questions[i].text + '</td>\n\
+</tr>';
+		    tbody.innerHTML += tr;
+		}
+	    } else {
 		return false;
 	    }
 	}
     };
     xhr.send();
-}
-document.getElementById('questions-btn').onclick = function(){
-    var questions = getQuestions();
-    console.log(questions);
 };
 
